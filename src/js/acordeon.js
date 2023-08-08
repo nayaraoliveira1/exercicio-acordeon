5ce52db15d28b9559013c0abde636eaf9c3bbb39
Nayara Oliveira <nayolliveira12@gmail.com>
2023-08-08 18:45:59 -0300

    adicionando arquivos

---
+++ src/js/acordeon.js
@@ -0,0 +1,11 @@
+const itensPerguntasERespostas = document.querySelectorAll(".item");
+
+itensPerguntasERespostas.forEach(function (item){
+    item.addEventListener("click", function () {
+        const itemAtivoAtual = document.querySelector(".ativo");
+        itemAtivoAtual.classList.remove("ativo");
+        
+        item.classList.add("ativo");
+    });
+});
+
